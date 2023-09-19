b796bd1841fb9962eaea5b69f9aa797b41be34ad
Parth Agarwal <agarwalparthsplendid@gmail.com>
2023-09-19 18:32:35 +0530

    branch commit hopefully

--- code.java
+++ code.java
@@ -2,5 +2,5 @@ class code
 {
     public static void main(String args[])
     {
-        System.out.print("hello world");    }
+        System.out.print("modified file for a new branch course");    }
 }
